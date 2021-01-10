4a72d99807d3a2491ac5fd8b1686f5ab87a38d4d
VOG <vambasternumero10@gmail.com>
2021-01-10 15:06:43 -0300

    Create front-web initial file

---
+++ front-web/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import reportWebVitals from './reportWebVitals';
+
+
+
+function App() {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
+}
+
+export default App;
